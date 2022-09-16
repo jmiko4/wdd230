169ecdca03bbdcabf0dc5d19658817cf5254f4f6
jmiko4 <jmiko4@byui.edu>
2022-09-16 10:29:59 -0600

    addded home page stuff

---
+++ js/home-page.js
@@ -0,0 +1,10 @@
+let date= new Date();
+
+let year = date.getFullYear();
+
+document.getElementById("footer").innerHTML = "© " + year + " .:|:. Justin J. Mikolajcik .:|:. Idaho";
+
+let text = document.lastModified;
+
+document.getElementById("update").innerHTML = "Last Updated: "+ text;
+
