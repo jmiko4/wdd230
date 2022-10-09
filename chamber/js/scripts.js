4c1c14d6226ce8d17e8a7039347dc695b5a836c3
jmiko4 <jmiko4@byui.edu>
2022-10-08 18:19:17 -0600

    thing

---
+++ chamber/js/scripts.js
@@ -0,0 +1,15 @@
+const hamburgerBtn = document.querySelector('.ham');
+const navigation = document.querySelector('.mobile-links');
+
+hamburgerBtn.addEventListener('click',() => {navigation.classList.toggle('responsive')});
+
+const date = new Date();
+const fullDate = new Intl.DateTimeFormat('en-US',{dateStyle: 'full'}).format(date._now);
+
+document.querySelector('#date-box').innerHTML = fullDate;
+
+const year = date.getFullYear();
+document.querySelector('#year').innerHTML = "© " +year;
+
+const text = document.lastModified;
+document.querySelector('#last-modified').innerHTML = text;
