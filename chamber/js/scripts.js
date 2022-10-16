8089125e18d594ed69e25c534e81170443935db5
jmiko4 <jmiko4@byui.edu>
2022-10-15 21:12:12 -0600

    did js stff

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -5,6 +5,8 @@ hamburgerBtn.addEventListener('click',() => {navigation.classList.toggle('respon
 
 const date = new Date();
 const fullDate = new Intl.DateTimeFormat('en-US',{dateStyle: 'full'}).format(date._now);
+const day = date.getDay();
+// let day = 1;
 
 document.querySelector('#date-box').innerHTML = fullDate;
 
@@ -13,3 +15,10 @@ document.querySelector('#year').innerHTML = "© " +year;
 
 const text = document.lastModified;
 document.querySelector('#last-modified').innerHTML = "Last Modified: " + text;
+
+const banner = document.querySelector('.banner');
+banner.innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+
+if (day == 1||day ==2){
+    banner.classList.toggle('display');
+}
