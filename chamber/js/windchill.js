2eb904710fe0552f5ef0f8b01681ecb24dc908cd
jmiko4 <jmiko4@byui.edu>
2022-10-22 16:44:37 -0600

    high sfhool musiclcal

---
+++ chamber/js/windchill.js
@@ -0,0 +1,12 @@
+const wind = document.getElementById('wind').textContent;
+const temp = document.getElementById('temp').textContent;
+
+if (temp < 50 && wind > 3) {
+    let chill = Math.round((35.74 + (0.6215 * temp)) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temp * Math.pow(wind, 0.16)));
+    document.getElementById('wind-chill').innerHTML = chill +"°F";
+    }
+else {
+    let chill = "NA";
+    document.getElementById('wind-chill').innerHTML = chill;}
+
+
